f0cb4e2e25b8c391a570c88a53517ede639021a5
Sercan Kocagoncu <alisk85@gmail.com>
2018-01-28 16:06:10 +0300

    initial commit

---
+++ src/main/java/com/missingartifacats/crm/SimpleCrmdemoApplication.java
@@ -0,0 +1,12 @@
+package com.missingartifacats.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrmdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrmdemoApplication.class, args);
+	}
+}
